3c801bbb93ab371bea98fb05a50ddaebfc94a518
Krystof Beuermann <kreiselkreisch@gmx.de>
2019-05-04 21:59:44 +0200

    update example app to elixir 1.4

--- test/support/apps/example/mix.exs
+++ test/support/apps/example/mix.exs
@@ -35,14 +35,16 @@ defmodule Example.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.3.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.16"},
-      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.7"},
+      {:jason, "~> 1.0"},
       {:torchstrap, path: "../../../../"}
     ]
   end
